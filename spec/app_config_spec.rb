9dae4c2d21bf3a67f3d20b01b18908fe291f17be
Shannon -jj Behrens <jjinux@gmail.com>
2012-02-07 12:21:20 -0800

    Added a scopes field

--- spec/app_config_spec.rb
+++ spec/app_config_spec.rb
@@ -4,5 +4,6 @@ describe APP_CONFIG do
   it "has OAuth2 configuration" do
     APP_CONFIG['oauth2']['client_id'].should_not be_nil
     APP_CONFIG['oauth2']['client_secret'].should_not be_nil
+    APP_CONFIG['oauth2']['scopes'].should_not be_nil
   end
 end
