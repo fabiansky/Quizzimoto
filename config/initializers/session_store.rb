7f58654f44cfeb393e8d0ae42cdf2f23f3f2e059
Shannon -jj Behrens <jjinux@gmail.com>
2012-02-07 09:41:16 -0800

    rails new quizzimoto

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Quizzimoto::Application.config.session_store :cookie_store, :key => '_quizzimoto_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Quizzimoto::Application.config.session_store :active_record_store
