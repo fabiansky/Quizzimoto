b46344edc1b2da69e46500ef65c014ff376d4095
Shannon -jj Behrens <jjinux@gmail.com>
2012-02-09 11:03:05 -0800

    OAuth2 forces you to login

--- spec/requests/oauth2_spec.rb
+++ spec/requests/oauth2_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 
 describe 'OAuth2' do
-  it 'forces you to login before you can see the welcome page' do
+  it 'forces you to login' do
     visit '/'
+    page.current_url.should == 'http://www.example.com/oauth2authorize'
+    page.should have_content('This application lets you build video-based quizzes')
     page.should have_content('Login to continue')
   end
 end
