7b3fb067ffea0690e2a8c35b4bf864db7f4b4369
Shannon -jj Behrens <jjinux@gmail.com>
2012-02-09 10:48:17 -0800

    Added tests concerning project layout and expected directories

---
+++ spec/project_layout_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe 'This project' do
+  it 'uses RSpec instead of TestUnit' do
+    Dir.exist?(File.join(File.dirname(__FILE__), '..', 'test')).should be_false
+  end
+
+  it 'uses request specs instead of controller and view specs' do
+    Dir.exist?(File.join(File.dirname(__FILE__), 'controllers')).should be_false
+    Dir.exist?(File.join(File.dirname(__FILE__), 'views')).should be_false
+    Dir.exist?(File.join(File.dirname(__FILE__), 'requests')).should be_true
+  end
+end
