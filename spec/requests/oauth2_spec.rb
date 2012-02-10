4b187677b0f086a825263a27e8837b2f55c7d7c9
Shannon -jj Behrens <jjinux@gmail.com>
2012-02-10 07:18:21 -0800

    Wrote some tests for the OAuth2 behavior

--- spec/requests/oauth2_spec.rb
+++ spec/requests/oauth2_spec.rb
@@ -7,4 +7,17 @@ describe 'OAuth2' do
     page.should have_content('This application lets you build video-based quizzes')
     page.should have_content('Login to continue')
   end
+
+  it 'lets you view the welcome page if you login' do
+    login
+    page.current_url.should == 'http://www.example.com/'
+    page.should have_content('This application lets you build video-based quizzes')
+    page.should_not have_content('Login to continue')
+  end
+
+  context '/oauth2callback' do
+    it 'crashes unless you pass a code parameter' do
+      lambda { visit '/oauth2callback' }.should raise_exception(ArgumentError)
+    end
+  end
 end
