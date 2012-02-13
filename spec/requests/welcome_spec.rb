6ead7f5f13c1a50c4b3a9d5c6b770c7282215d67
Shannon -jj Behrens <jjinux@gmail.com>
2012-02-13 07:55:38 -0800

    Started working on a new test; it's not done

--- spec/requests/welcome_spec.rb
+++ spec/requests/welcome_spec.rb
@@ -7,4 +7,12 @@ describe 'Welcome' do
       page.current_url.should == 'http://www.example.com/oauth2authorize'
     end
   end
+
+  it "shows a list of quizzes" do
+    Factory :quiz
+    visit '/'
+    page.should have_content('Factoring Primes')
+    page.should have_content('13')
+    page.should have_content('United States')
+  end
 end
