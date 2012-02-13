68ed297df5c3bdf118f6f10fb16c5b1857daec30
Shannon -jj Behrens <jjinux@gmail.com>
2012-02-13 10:40:56 -0800

    Merged the welcome controller into the quizzes controller

--- spec/project_layout_spec.rb
+++ spec/project_layout_spec.rb
@@ -5,6 +5,11 @@ describe 'This project' do
     Dir.exist?(File.join(File.dirname(__FILE__), '..', 'test')).should be_false
   end
 
+  it 'prefers request specs with Capybara over view specs' do
+     Dir.exist?(File.join(File.dirname(__FILE__), 'views')).should be_false
+     Dir.exist?(File.join(File.dirname(__FILE__), 'requests')).should be_true
+   end
+
   it 'uses the countries gem' do
     Country.new('US').should_not be_nil
   end
