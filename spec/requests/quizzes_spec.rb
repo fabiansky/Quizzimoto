d209b6b21a823d602f4bb66481ad6bd01d0d4b59
Shannon -jj Behrens <jjinux@gmail.com>
2012-02-13 10:07:09 -0800

    Merged the scaffolding-generated tests with my own tests

--- spec/requests/quizzes_spec.rb
+++ spec/requests/quizzes_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe "Quizzes" do
   describe "GET /quizzes" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get quizzes_path
-      response.status.should be(200)
+    it "doesn't crash" do
+      login
+      visit quizzes_path
+      page.should have_content('Listing quizzes')
     end
   end
 end
