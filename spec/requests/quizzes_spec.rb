c4a6ce3712a238417dcc6d1eb0913688d861b872
Shannon -jj Behrens <jjinux@gmail.com>
2012-02-13 10:40:41 -0800

    Merged the welcome controller into the quizzes controller

--- spec/requests/quizzes_spec.rb
+++ spec/requests/quizzes_spec.rb
@@ -1,11 +1,24 @@
 require 'spec_helper'
 
 describe "Quizzes" do
-  describe "GET /quizzes" do
-    it "doesn't crash" do
-      login
-      visit quizzes_path
-      page.should have_content('Listing quizzes')
+  %w(/ /quizzes).each do |path|
+    it 'should redirect you to login' do
+      visit path
+      page.current_url.should == 'http://www.example.com/oauth2authorize'
     end
+  end
+
+  it 'shows a list of quizzes' do
+    login
+    Factory :quiz
+    visit '/'
+    page.should have_content('Scrabble for Nihilists')
+    page.should have_content('47')
+    page.should have_content('United States')
+  end
+
+  it 'says something when there are no quizzes' do
+    login
+    page.should have_content('There are no quizzes yet')
   end
 end
