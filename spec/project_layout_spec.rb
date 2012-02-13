882efff2370bf4be39c2c4af3d6149245a70117e
Shannon -jj Behrens <jjinux@gmail.com>
2012-02-13 10:04:02 -0800

    Merged the scaffolding-generated tests with my own tests

--- spec/project_layout_spec.rb
+++ spec/project_layout_spec.rb
@@ -5,12 +5,6 @@ describe 'This project' do
     Dir.exist?(File.join(File.dirname(__FILE__), '..', 'test')).should be_false
   end
 
-  it 'uses request specs instead of controller and view specs' do
-    Dir.exist?(File.join(File.dirname(__FILE__), 'controllers')).should be_false
-    Dir.exist?(File.join(File.dirname(__FILE__), 'views')).should be_false
-    Dir.exist?(File.join(File.dirname(__FILE__), 'requests')).should be_true
-  end
-
   it 'uses the countries gem' do
     Country.new('US').should_not be_nil
   end
