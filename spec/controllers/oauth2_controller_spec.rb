114b3c8272694c115b4211405010765cacbde359
Shannon -jj Behrens <jjinux@gmail.com>
2012-02-15 14:28:48 -0800

    Improved how OAuth2 is done
    Made it more like https://oauthssodemo.appspot.com/ by tweaking some
    parameters.

---
+++ spec/controllers/oauth2_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe Oauth2Controller do
+  describe 'GET authorize' do
+    it 'generates the correct authorization_uri' do
+      get :authorize, {}, {}
+      authorization_uri = assigns(:authorization_uri)
+      params = CGI.parse(authorization_uri.query)
+      params['access_type'].first.should == 'offline'
+      params['approval_prompt'].first.should == 'auto'
+      params['scope'].first.split.should == %w(
+        https://www.googleapis.com/auth/userinfo.email
+        https://www.googleapis.com/auth/userinfo.profile
+        http://gdata.youtube.com  
+      )
+    end
+  end
+end
