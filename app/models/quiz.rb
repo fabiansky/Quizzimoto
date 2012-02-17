f4e952cb203dfc5dc1c5c1c1c2228dd2ae771ebd
Shannon -jj Behrens <jjinux@gmail.com>
2012-02-17 13:44:08 -0800

    A quiz must have an owner_id

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,6 +1,6 @@
 class Quiz < ActiveRecord::Base
-  validates :name, :playlist_id, :min_age_years, :country_alpha2,
-    :presence => true
+  validates :name, :owner_id, :playlist_id, :min_age_years,
+    :country_alpha2, :presence => true
   validates :min_age_years,
     :numericality => { :greater_than_or_equal_to => 0 }
   validates :country_alpha2,
