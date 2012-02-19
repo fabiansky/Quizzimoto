c0fc58b6e7da2ab3be75986107267585cdc83597
Shannon -jj Behrens <jjinux@gmail.com>
2012-02-18 20:40:50 -0800

    Show the embedded player

--- spec/requests/quizzes_spec.rb
+++ spec/requests/quizzes_spec.rb
@@ -13,45 +13,64 @@ describe "Quizzes" do
     end
   end
 
-  it 'shows a list of quizzes' do
-    Factory :quiz
-    login
-    page.should have_content('Scrabble for Nihilists')
-    page.should have_content('47')
-    page.should have_content('United States')
-  end
+  describe 'index' do
+    it 'shows a list of quizzes' do
+      Factory :quiz
+      login
+      page.should have_content('Scrabble for Nihilists')
+      page.should have_content('47')
+      page.should have_content('United States')
+    end
 
-  it 'says something when there are no quizzes' do
-    login
-    page.should have_content('There are no quizzes yet')
-  end
+    it 'says something when there are no quizzes' do
+      login
+      page.should have_content('There are no quizzes yet')
+    end
 
-  it 'allows you to create a new quiz' do
-    login
-    click_link 'Create a new quiz'
-    page.should have_content('New quiz')
-  end
-
-  # Advanced validation is tested in models/quiz_spec.rb.
-
-  it 'does validation' do
-    login
-    stub_plus_discovery_document
-    stub_current_user_profile
-    visit new_quiz_path
-    click_button 'Create Quiz'
-    ['Name', 'Country alpha2'].each do |field|
-      page.should have_content("#{field} can't be blank")
+    it 'allows you to create a new quiz' do
+      login
+      click_link 'Create a new quiz'
+      page.should have_content('New quiz')
     end
   end
 
-  it 'lets you edit a quiz' do
-    Factory :quiz
-    login
-    click_link 'Scrabble for Nihilists'
-    click_link 'Edit'
-    fill_in 'Name', :with => 'Scrabble for bad spelers'
-    click_button 'Update Quiz'
-    page.should have_content('Scrabble for bad spelers')
+  describe 'edit' do
+    # Advanced validation is tested in models/quiz_spec.rb.
+    it 'does validation' do
+      login
+      stub_plus_discovery_document
+      stub_current_user_profile
+      visit new_quiz_path
+      click_button 'Create Quiz'
+      ['Name', 'Country alpha2'].each do |field|
+        page.should have_content("#{field} can't be blank")
+      end
+    end
+  end
+
+  describe 'show' do
+    it 'lets you edit a quiz' do
+      Factory :quiz
+      login
+      click_link 'Scrabble for Nihilists'
+      click_link 'Edit'
+      fill_in 'Name', :with => 'Scrabble for bad spelers'
+      click_button 'Update Quiz'
+      page.should have_content('Scrabble for bad spelers')
+    end
+
+    it 'shows an embedded player' do
+      Factory :quiz
+      login
+      click_link 'Scrabble for Nihilists'
+      page.should have_css('iframe.player')
+    end
+
+    it 'tells you if there is no video' do
+      Factory :quiz, :video_id => nil
+      login
+      click_link 'Scrabble for Nihilists'
+      page.should have_content('This is no video associated with this quiz yet.')
+    end
   end
 end
