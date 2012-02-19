f462090bc5dc3695e1813bbc0d5911249f53d71d
Shannon -jj Behrens <jjinux@gmail.com>
2012-02-18 18:53:51 -0800

    Use a YouTube developer key

--- spec/app_config_spec.rb
+++ spec/app_config_spec.rb
@@ -1,9 +1,13 @@
 require 'spec_helper'
 
 describe APP_CONFIG do
-  it "has OAuth2 configuration" do
+  it 'has OAuth2 configuration' do
     APP_CONFIG['oauth2']['client_id'].should_not be_nil
     APP_CONFIG['oauth2']['client_secret'].should_not be_nil
     APP_CONFIG['oauth2']['scopes'].should_not be_nil
+  end
+
+  it 'has YouTube configuration' do
+    APP_CONFIG['youtube']['dev_key'].should_not be_nil
   end
 end
