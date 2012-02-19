b50f55240f9f2187ae59af05d01a4cc8a4fc548e
Shannon -jj Behrens <jjinux@gmail.com>
2012-02-18 20:25:47 -0800

    Changed the video_id in the factory

--- spec/factories/quizzes.rb
+++ spec/factories/quizzes.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
   factory :quiz do
     name 'Scrabble for Nihilists'
     owner_id 'owner_id'
-    video_id 'video_id'
+    video_id '4RjsedD_Qjc'
     min_age_years '47'
     country_alpha2 'US'
   end
