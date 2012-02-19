4d24cafed80e4d801215a7835969d27167364fc6
Shannon -jj Behrens <jjinux@gmail.com>
2012-02-18 21:55:13 -0800

    The header should be clickable

--- spec/requests/layout_spec.rb
+++ spec/requests/layout_spec.rb
@@ -5,12 +5,16 @@ describe 'Layout' do
     stub_plus_discovery_document
     stub_current_user_profile
   end
+  
+  it 'has a link to the homepage' do
+    login
+    click_link 'Quizzimoto'
+    page.current_url.should == 'http://www.example.com/'
+  end
 
-  describe 'After logging in' do
-    it 'Show stuff specific to the current user' do
-      login
-      page.should have_content('Shannon Behrens')
-      page.should have_css('#profile_pic img')
-    end
+  it 'shows stuff specific to the current user' do
+    login
+    page.should have_content('Shannon Behrens')
+    page.should have_css('#profile_pic img')
   end
 end
