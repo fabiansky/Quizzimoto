ea4f77011d210e864e348ffe4933583fcaa9fff3
Shannon -jj Behrens <jjinux@gmail.com>
2012-02-20 22:31:00 -0800

    Created a basic factory and a complete factory

--- spec/factories/quizzes.rb
+++ spec/factories/quizzes.rb
@@ -4,9 +4,16 @@ FactoryGirl.define do
   factory :quiz do
     name 'Scrabble for Nihilists'
     owner_id 'owner_id'
-    video_id '4RjsedD_Qjc'
-    form_id 'dEI4TWtYaklKX0NTRnFBTC1SRWRscWc6MQ'
     min_age_years '47'
+    country_alpha2 'US'
+  end
+
+  factory :complete_quiz, :class => Quiz do
+    name 'Trigonometry'
+    owner_id 'owner_id'
+    video_id '0HYHG3fuzvk'
+    form_id 'dEI4TWtYaklKX0NTRnFBTC1SRWRscWc6MQ'
+    min_age_years '14'
     country_alpha2 'US'
   end
 end
