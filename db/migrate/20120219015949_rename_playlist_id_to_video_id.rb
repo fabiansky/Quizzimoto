250ed0aba5e975b2ee81f64318492d1aa51fdb20
Shannon -jj Behrens <jjinux@gmail.com>
2012-07-30 11:00:11 -0700

    Added copyrights

--- db/migrate/20120219015949_rename_playlist_id_to_video_id.rb
+++ db/migrate/20120219015949_rename_playlist_id_to_video_id.rb
@@ -1,3 +1,17 @@
+# Copyright 2012 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # A quiz should contain a video_id, not a playlist_id.  This simplifies the
 # app quite a bit.  At this point, it's safe to assume there aren't any real
 # playlist_ids in the database, so renaming the column is the simplest thing
