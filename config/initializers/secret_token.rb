9c2dea9fd294b17ddbd8154af714c3174659c597
Juanito Fatas <katehuang0320@gmail.com>
2013-08-16 20:39:33 +0800

    Use token from YAML

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -18,4 +18,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Quizzimoto::Application.config.secret_token = '1b6935e3c7d5d1b0fe90f62526f3c8debbbbc1ea694fb8ab2f0d562dda3a1d0a5c11fed0d38e7597d104fcda661a2da653448ae7fff0f024f063dd556c1d1618'
+Quizzimoto::Application.config.secret_token = APP_CONFIG['secret_token']
